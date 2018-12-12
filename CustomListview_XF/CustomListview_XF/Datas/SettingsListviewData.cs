7b7f9ad1f5b1c2e7179d9de876d08ad18f99db0d
Elif Aybike Aslan <elfaybke@gmail.com>
2018-12-12 17:42:32 +0300

    Custom Listview Yapısı Eklendi

---
+++ CustomListview_XF/CustomListview_XF/Datas/SettingsListviewData.cs
@@ -0,0 +1,56 @@
+﻿using CustomListview_XF.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomListview_XF.Datas
+{
+    public class SettingsListviewData
+    {
+        public List<SettingsListviewModel> SettingsListviewModels = new List<SettingsListviewModel>()
+        {
+             new SettingsListviewModel
+            {
+                SettingsID=1,
+                Settings="Bildirim Ayarları",
+                SettingsImage="ileri.png",
+                SettingsIcon="bildirimayarlari.png"
+            },
+            new SettingsListviewModel
+            {
+                SettingsID=2,
+                Settings="Yardım Merkezi",
+                SettingsImage="ileri.png",
+                SettingsIcon="yardimmerkezi.png"
+            },
+            new SettingsListviewModel
+            {
+                SettingsID=3,
+                Settings="Hakkımızda",
+                SettingsImage="ileri.png",
+                SettingsIcon="hakkimizda.png"
+            },
+            new SettingsListviewModel
+            {
+                SettingsID=4,
+                Settings="Sorun Bildir",
+                SettingsImage="ileri.png",
+                SettingsIcon="sorunbildir.png"
+            },
+            new SettingsListviewModel
+            {
+                SettingsID=5,
+                Settings="Canlı Destek",
+                SettingsImage="ileri.png",
+                SettingsIcon="canlidestek.png"
+            },
+            new SettingsListviewModel
+            {
+                SettingsID=6,
+                Settings="Çıkış",
+                SettingsImage="ileri.png",
+                SettingsIcon="cikis.png"
+            }
+        };
+    }
+}
---
+++ CustomListview_XF/CustomListview_XF/Models/SettingsListviewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomListview_XF.Models
+{
+    public class SettingsListviewModel
+    {
+        public int SettingsID { get; set; }
+
+        public String Settings { get; set; }
+
+        public String SettingsImage { get; set; }
+
+        public String SettingsIcon { get; set; }
+
+    }
+}
